7b7f6b1c34cb616338a6e3850d93366169c8c8b7
cjonesey <cjonesey@live.com>
2024-03-21 13:24:09 +0000

    UPDATED ui

---
+++ Phoenix.Models.Shared/SearchModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phoenix.Models.Shared
+{
+    public class SearchModel
+    {
+        public Guid Id { get; set; }
+        public string FieldName { get; set; }
+        public ConnectorType Connector { get; set; }
+        public string Value { get; set; }
+    }
+
+    public enum ConnectorType
+    {
+        And = 0,
+        Or = 1
+    }
+}
